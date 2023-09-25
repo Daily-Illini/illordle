6375cabd24db71ac9837662b2d47c81981a18d21
Prashant Naganaboyina <pkn3@illinois.edu>
2023-09-25 15:04:19 -0500

    add game win/lose state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 import Board from "./components/Board";
+import GameStateModal from "./components/GameStateModal";
 
 function App() {
+  const [gameWin, setGameWin] = useState(false);
+  const [gameLose, setGameLose] = useState(false);
+
+  function winGame() {
+    setGameWin(true);
+  }
+
+  function loseGame() {
+    setGameLose(true);
+  }
+
   return (
     <div className="grid place-items-center">
+      {(gameWin || gameLose) && <GameStateModal didWin={gameWin} />}
+
       <h1 className="m-2 font-bold text-2xl">Illordle</h1>
-      <Board word={"illini"} />
+      <Board word={"illini"} win={winGame} lose={loseGame} />
     </div>
   );
 }
